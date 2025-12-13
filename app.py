6891450e021f1c3f5f19bb0720b91e58cca42813
cvargaspimentel157-star <cvargaspimentel157@gmail.com>
2025-12-13 10:17:55 -0500

    cambio2

--- app.py
+++ app.py
@@ -37,7 +37,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 )
 
 # Configuración de la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://usuario:@localhost/delicake'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
