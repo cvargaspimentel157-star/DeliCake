d19b657c5cce86802a2d0b6b301c30b75f9b0614
cvargaspimentel157-star <cvargaspimentel157@gmail.com>
2025-12-17 20:52:25 -0500

    cambio10

--- controladores/models.py
+++ controladores/models.py
@@ -3,12 +3,12 @@ from flask_login import UserMixin
 from datetime import datetime
 from sqlalchemy.orm import relationship
 
-
 db = SQLAlchemy()
 
 
 class Usuario(db.Model, UserMixin):
     __tablename__ = "usuario"
+
     ID_usuario = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Nombre = db.Column(db.String(100), nullable=False)
     Apellido = db.Column(db.String(100), nullable=False)
@@ -25,10 +25,11 @@ class Usuario(db.Model, UserMixin):
 
 class Cliente(db.Model):
     __tablename__ = "cliente"
+
     ID_cliente = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Direccion = db.Column(db.String(200))
     Telefono = db.Column(db.String(20))
-    ID_usuario = db.Column(db.Integer, db.ForeignKey("Usuario.ID_usuario"))
+    ID_usuario = db.Column(db.Integer, db.ForeignKey("usuario.ID_usuario"))
 
     usuario = db.relationship("Usuario", back_populates="cliente")
     pedidos = db.relationship("Pedido", back_populates="cliente")
@@ -37,14 +38,12 @@ class Cliente(db.Model):
     pagos = relationship("Pago", back_populates="cliente")
 
 
-
-
-
 class Administrador(db.Model):
     __tablename__ = "administrador"
+
     ID_admin = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Rol = db.Column(db.String(50))
-    ID_usuario = db.Column(db.Integer, db.ForeignKey("Usuario.ID_usuario"))
+    ID_usuario = db.Column(db.Integer, db.ForeignKey("usuario.ID_usuario"))
     Clave_validacion = db.Column(db.Integer)
 
     usuario = db.relationship("Usuario", back_populates="administrador")
@@ -52,7 +51,7 @@ class Administrador(db.Model):
 
 
 class Producto(db.Model):
-    __tablename__ = 'producto'
+    __tablename__ = "producto"
 
     ID_Producto = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Nombre_producto = db.Column(db.String(255), nullable=False)
@@ -63,14 +62,15 @@ class Producto(db.Model):
     Imagen = db.Column(db.String(255))
     Precio_descuento = db.Column(db.Float, nullable=True)
     Descuento = db.Column(db.Numeric(5, 2), default=0.00)
-    ID_Categoria = db.Column(db.Integer, db.ForeignKey('Categoria.ID_Categoria'), nullable=False)
-    ID_Administrador = db.Column(db.Integer, db.ForeignKey('Administrador.ID_admin'), nullable=False)
+
+    ID_Categoria = db.Column(db.Integer, db.ForeignKey("categoria.ID_Categoria"), nullable=False)
+    ID_Administrador = db.Column(db.Integer, db.ForeignKey("administrador.ID_admin"), nullable=False)
 
     administrador = db.relationship("Administrador", back_populates="productos")
     categoria = db.relationship("Categoria", back_populates="productos")
     personalizaciones = db.relationship("PersonalizacionProducto", back_populates="producto")
     detalles_pedido = db.relationship("DetallePedido", back_populates="producto", cascade="all, delete-orphan")
-    detalles = db.relationship('DetalleProducto', back_populates='producto', cascade='all, delete-orphan')
+    detalles = db.relationship("DetalleProducto", back_populates="producto", cascade="all, delete-orphan")
     calificaciones = db.relationship("Calificacion", back_populates="producto")
     pedidos = db.relationship("Pedido", back_populates="producto")
 
@@ -85,31 +85,30 @@ class Pedido(db.Model):
     Estado_Pedido = db.Column(db.Enum('Pendiente', 'En proceso', 'Enviado', 'Entregado', 'Cancelado'), default='Pendiente')
     Total = db.Column(db.Numeric(10, 2))
     Metodo_Pago = db.Column(db.Enum('Tarjeta', 'Nequi', 'Daviplata', 'Efectivo'), nullable=True)
-    ID_Cliente = db.Column(db.Integer, db.ForeignKey("Cliente.ID_cliente"), nullable=False)
-    ID_Producto = db.Column(db.Integer, db.ForeignKey("Producto.ID_Producto"), nullable=False)
+
+    ID_Cliente = db.Column(db.Integer, db.ForeignKey("cliente.ID_cliente"), nullable=False)
+    ID_Producto = db.Column(db.Integer, db.ForeignKey("producto.ID_Producto"), nullable=False)
 
     cliente = db.relationship("Cliente", back_populates="pedidos")
     producto = db.relationship("Producto", back_populates="pedidos")
     detalles_pedido = db.relationship("DetallePedido", back_populates="pedido", cascade="all, delete-orphan")
     personalizaciones = db.relationship("PersonalizacionProducto", back_populates="pedido", cascade="all, delete-orphan")
     pagos = db.relationship("Pago", back_populates="pedido", cascade="all, delete")
-     
-
-
-
 
 
 class PersonalizacionProducto(db.Model):
-    __tablename__ = "personalizacion_Producto"
+    __tablename__ = "personalizacion_producto"
+
     ID_Personalizacion = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Masa = db.Column(db.Enum('batida', 'azucarada', 'fermentadas'), default='batida')
     Relleno = db.Column(db.Enum('vainilla', 'dulce de leche', 'nueces', 'crema de limon'))
     Cobertura = db.Column(db.Enum('chocolate', 'maracuya', 'vainilla', 'fresa', 'melocoton'))
     Porciones = db.Column(db.Enum('1 porcion', '2 porciones', '1/2', 'entero', '3 a 5 porciones'))
     Adicionales = db.Column(db.Enum('chantilly', 'leche condensada', 'arequipe ', 'chocolate', 'fresa'))
-    ID_Producto = db.Column(db.Integer, db.ForeignKey("Producto.ID_Producto"))
-    ID_Cliente = db.Column(db.Integer, db.ForeignKey("Cliente.ID_cliente"))
-    ID_Pedido = db.Column(db.Integer, db.ForeignKey("Pedido.ID_Pedido"))
+
+    ID_Producto = db.Column(db.Integer, db.ForeignKey("producto.ID_Producto"))
+    ID_Cliente = db.Column(db.Integer, db.ForeignKey("cliente.ID_cliente"))
+    ID_Pedido = db.Column(db.Integer, db.ForeignKey("pedido.ID_Pedido"))
 
     producto = db.relationship("Producto", back_populates="personalizaciones")
     cliente = db.relationship("Cliente", back_populates="personalizaciones")
@@ -118,7 +117,7 @@ class PersonalizacionProducto(db.Model):
 
 
 class DetallePedido(db.Model):
-    __tablename__ = "detalle_Pedido"
+    __tablename__ = "detalle_pedido"
 
     ID_detalle_pedido = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Nombre = db.Column(db.String(29))
@@ -133,36 +132,38 @@ class DetallePedido(db.Model):
     Adicionales = db.Column(db.Enum('chantilly', 'leche condensada', 'arequipe', 'chocolate', 'fresa'))
     Precio_Unitario = db.Column(db.Numeric(10, 2))
     Total = db.Column(db.Numeric(10, 2))
-    ID_Pedido = db.Column(db.Integer, db.ForeignKey("Pedido.ID_Pedido"), nullable=False)
-    ID_Producto = db.Column(db.Integer, db.ForeignKey("Producto.ID_Producto"), nullable=False)
-    ID_Cliente = db.Column(db.Integer, db.ForeignKey("Cliente.ID_cliente"), nullable=False)
-    ID_Personalizacion = db.Column(db.Integer, db.ForeignKey("Personalizacion_Producto.ID_Personalizacion"))
+
+    ID_Pedido = db.Column(db.Integer, db.ForeignKey("pedido.ID_Pedido"), nullable=False)
+    ID_Producto = db.Column(db.Integer, db.ForeignKey("producto.ID_Producto"), nullable=False)
+    ID_Cliente = db.Column(db.Integer, db.ForeignKey("cliente.ID_cliente"), nullable=False)
+    ID_Personalizacion = db.Column(db.Integer, db.ForeignKey("personalizacion_producto.ID_Personalizacion"))
 
     pedido = db.relationship("Pedido", back_populates="detalles_pedido")
     producto = db.relationship("Producto", back_populates="detalles_pedido")
     cliente = db.relationship("Cliente", back_populates="detalles_pedido")
     personalizacion = db.relationship("PersonalizacionProducto", back_populates="detalles_pedido")
-    
 
 
 class Categoria(db.Model):
-    __tablename__ = 'categoria'
+    __tablename__ = "categoria"
+
     ID_Categoria = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Nombre_categoria = db.Column(db.String(20))
     Nombre_producto = db.Column(db.String(40))
     Estado = db.Column(db.Enum('Activa', 'Inactiva'))
-    productos = db.relationship('Producto', back_populates='categoria', lazy=True)
+
+    productos = db.relationship("Producto", back_populates="categoria", lazy=True)
 
 
 class DetalleProducto(db.Model):
-    __tablename__ = 'detalle_Producto'
+    __tablename__ = "detalle_producto"
+
     ID_detalle_producto = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Ingredientes = db.Column(db.String(70))
     tiempo_preparacion = db.Column(db.String(50))
-    ID_producto = db.Column(db.Integer, db.ForeignKey('Producto.ID_Producto', ondelete='CASCADE'), nullable=False)
-    producto = db.relationship('Producto', back_populates='detalles')
+    ID_producto = db.Column(db.Integer, db.ForeignKey("producto.ID_Producto", ondelete="CASCADE"), nullable=False)
 
-
+    producto = db.relationship("Producto", back_populates="detalles")
 
 
 class Pago(db.Model):
@@ -173,23 +174,18 @@ class Pago(db.Model):
     Metodo_Pago = db.Column(db.String(50))
     Estado_Pago = db.Column(db.Enum('Pendiente', 'Aprobado', 'Rechazado'), default='Pendiente')
     fecha_pago = db.Column(db.Date)
-    Mensaje_Cliente = db.Column(db.String(255), default="Espera un momento, el administrador está revisando") 
+    Mensaje_Cliente = db.Column(db.String(255), default="Espera un momento, el administrador está revisando")
 
-    ID_Cliente = db.Column(db.Integer, db.ForeignKey("Cliente.ID_cliente"), nullable=False)
-    ID_Pedido = db.Column(db.Integer, db.ForeignKey("Pedido.ID_Pedido"), nullable=True)
+    ID_Cliente = db.Column(db.Integer, db.ForeignKey("cliente.ID_cliente"), nullable=False)
+    ID_Pedido = db.Column(db.Integer, db.ForeignKey("pedido.ID_Pedido"), nullable=True)
 
-
-    # Relaciones
     cliente = relationship("Cliente", back_populates="pagos")
     pedido = relationship("Pedido", back_populates="pagos")
-   
-
-
-
 
 
 class Disponibilidad(db.Model):
     __tablename__ = "disponibilidad"
+
     ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Fecha = db.Column(db.Date, nullable=False)
     Hora = db.Column(db.Time, nullable=False)
@@ -197,50 +193,59 @@ class Disponibilidad(db.Model):
 
 class Calificacion(db.Model):
     __tablename__ = "calificacion"
+
     ID_Calificacion = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    ID_Producto = db.Column(db.Integer, db.ForeignKey("Producto.ID_Producto"), nullable=False)
+    ID_Producto = db.Column(db.Integer, db.ForeignKey("producto.ID_Producto"), nullable=False)
     Valor = db.Column(db.Integer, nullable=False)
+
     producto = db.relationship("Producto", back_populates="calificaciones")
 
 
 class Reseña(db.Model):
     __tablename__ = "reseña"
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     correo = db.Column(db.String(100), nullable=False)
     estrellas = db.Column(db.Integer, nullable=False)
     comentario = db.Column(db.Text, nullable=False)
     estado = db.Column(db.String(20), default="pendiente")
     fecha = db.Column(db.DateTime, default=datetime.utcnow)
-    ID_usuario = db.Column(db.Integer, db.ForeignKey("Usuario.ID_usuario", ondelete='CASCADE'), nullable=False)
+    ID_usuario = db.Column(db.Integer, db.ForeignKey("usuario.ID_usuario", ondelete="CASCADE"), nullable=False)
 
 
 class Suscriptor(db.Model):
     __tablename__ = "suscriptor"
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     correo = db.Column(db.String(120), unique=True, nullable=False)
 
 
 class Notificacion(db.Model):
     __tablename__ = "notificacion"
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    usuario_id = db.Column(db.Integer, db.ForeignKey("Usuario.ID_usuario"), nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey("usuario.ID_usuario"), nullable=False)
     mensaje = db.Column(db.Text, nullable=False)
     fecha = db.Column(db.DateTime, default=db.func.now())
     leida = db.Column(db.Boolean, default=False)
+
     usuario = db.relationship("Usuario", back_populates="notificaciones")
 
 
 class Favorito(db.Model):
     __tablename__ = "favoritos"
+
     id_favorito = db.Column(db.Integer, primary_key=True)
-    ID_usuario = db.Column(db.Integer, db.ForeignKey('Usuario.ID_usuario'), nullable=False)
-    ID_Producto = db.Column(db.Integer, db.ForeignKey('Producto.ID_Producto'), nullable=False)
-    usuario = db.relationship('Usuario', backref='favoritos', lazy=True)
-    producto = db.relationship('Producto', backref='favoritos', lazy=True)
+    ID_usuario = db.Column(db.Integer, db.ForeignKey("usuario.ID_usuario"), nullable=False)
+    ID_Producto = db.Column(db.Integer, db.ForeignKey("producto.ID_Producto"), nullable=False)
+
+    usuario = db.relationship("Usuario", backref="favoritos", lazy=True)
+    producto = db.relationship("Producto", backref="favoritos", lazy=True)
 
 
 class Lanzamiento(db.Model):
-    __tablename__ = 'lanzamiento'
+    __tablename__ = "lanzamiento"
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     descripcion = db.Column(db.String(255), nullable=False)
     fecha_catalogo = db.Column(db.Date, nullable=False)
@@ -248,12 +253,10 @@ class Lanzamiento(db.Model):
 
 
 class FooterConfig(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "footer_config"
 
+    id = db.Column(db.Integer, primary_key=True)
     horario_1 = db.Column(db.String(200))
     horario_2 = db.Column(db.String(200))
     whatsapp_url = db.Column(db.String(500))
     ubicacion = db.Column(db.String(300))
-
-
-    
